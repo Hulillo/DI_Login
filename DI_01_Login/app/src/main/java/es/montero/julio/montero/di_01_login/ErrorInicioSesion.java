3af66b5d76aa237b194a6d6f66c7ce511700a17f
DESKTOP-IEN9EFP\zeisd <jmontben@myuax.com>
2023-10-17 14:19:46 +0200

    Primera Versión

---
+++ DI_01_Login/app/src/main/java/es/montero/julio/montero/di_01_login/ErrorInicioSesion.java
@@ -0,0 +1,14 @@
+package es.montero.julio.montero.di_01_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ErrorInicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_error_inicio_sesion);
+    }
+}
---
+++ DI_01_Login/app/src/main/java/es/montero/julio/montero/di_01_login/SignIn.java
@@ -0,0 +1,14 @@
+package es.montero.julio.montero.di_01_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
